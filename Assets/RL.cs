baa7ab79eebfc8d463ea6128083398b2e2dba3ea
Daniel Beckman <dbeckman@usgs.gov>
2020-12-08 02:07:20 -0700

    Add files via upload

---
+++ Assets/RL.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RL : MonoBehaviour
+{
+    public GameObject Cam;
+    public GameObject target;
+    public GameObject intercept;
+    public GameObject inter;
+    //private List<float> A = new List<float>();
+    //private List<float> B = new List<float>();
+    //private List<float> C = new List<float>();
+    //private List<float> D = new List<float>();
+    //private List<float> E = new List<float>();
+    private List<float> move = new List<float>();
+    //private List<List<float>> actionSpace = new List<List<float>>();
+    //private bool Guardian = false;
+    private List<int> Guardian = new List<int>();
+    private float exploreRate = 100f;
+    public int act;
+
+    //For all the marbles, the Q (S, A) Matrix
+    public List<List<float>>[,] QSA = new List<List<float>>[19, 7];
+
+
+
+
+
+
+    private void Start()
+    {
+        //GameObject.FindObjectWithTag("target").GetComponent<RandMove>().tick;
+        Guardian = target.GetComponent<RandMove>().tick;
+
+       
+        //temp[0] = 1f;
+        //Debug.Log(actionSpace[1][0]);
+        //QSA[17, 5] = actionSpace;
+
+        setQSpace();
+    }
+
+    private void Update()
+    {
+        if (Guardian[0] == 0)
+        {
+            //moveCam(B);
+            act = Random.Range(0, 5);
+            //Debug.Log(act);
+            EGA(act);
+            Guardian[0] = 1;
+        }
+        //Debug.Log(QSA[0,0][4][1]);
+
+    }
+
+    private void moveCam(List<float> Choice)
+    {
+        Cam.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        //inter = Instantiate(intercept, new Vector3(.0f, .8f, 1f), Quaternion.identity);
+        //inter.transform.SetParent(Cam.transform);
+        Cam.transform.Rotate(new Vector3(Choice[1], Choice[2], 0f));
+        //Destroy(inter);
+        
+
+    }
+
+    void EGA(int act)
+    {
+        if (Random.Range(0.00f, 100.00f) < exploreRate)
+        //if (Random.Range(0, 10) > 5)
+        {
+            exploreRate -= exploreRate * .005f;
+            explore(act);
+        }
+        else
+            exploit();
+        //count();
+        //Debug.Log(exploreRate);
+
+    }
+
+    void setQSpace()
+    {
+        //actionSpace.initialize_order();
+        for (int i = 0; i < 19; i++)
+            for (int ii = 0; ii < 7; ii++)
+            {
+                var aS = new List<List<float>>();
+
+                var temp = new List<float>();
+                temp.Add(0f);
+                temp.Add(-14f);
+                temp.Add(-33f);
+                aS.Add(temp);
+
+                temp = new List<float>();
+                temp.Add(0f);
+                temp.Add(14f);
+                temp.Add(33f);
+                aS.Add(temp);
+
+                temp = new List<float>();
+                temp.Add(0f);
+                temp.Add(14f);
+                temp.Add(-33f);
+                aS.Add(temp);
+
+                temp = new List<float>();
+                temp.Add(0f);
+                temp.Add(-14f);
+                temp.Add(33f);
+                aS.Add(temp);
+
+                temp = new List<float>();
+                temp.Add(0f);
+                temp.Add(0f);
+                temp.Add(0f);
+                aS.Add(temp);
+
+                QSA[i,ii] = aS;
+            }
+    }
+
+
+    void explore(int action)
+    {
+        Debug.Log("Method: EXPLORE");
+        move = QSA[Guardian[1], Guardian[2]][act];
+        //inter = Instantiate(intercept, new Vector3(.0f, .8f, 1f), Quaternion.identity);
+        if (action == 4)
+        {
+            //Cam.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            //inter.transform.SetParent(Cam.transform);
+            inter = Instantiate(intercept, new Vector3(.0f, 1f, 1f), Quaternion.identity);
+            inter.transform.localScale = new Vector3(.5f, 1f, 2f);
+        }
+        else if (action == 2)
+        {
+            inter = Instantiate(intercept, new Vector3(-.5f, .8f, 1f), Quaternion.identity);
+            inter.transform.localScale = new Vector3(.5f, .5f, 2f);
+        }
+        else if (action == 1)
+        {
+            inter = Instantiate(intercept, new Vector3(.5f, .8f, 1f), Quaternion.identity);
+            inter.transform.localScale = new Vector3(.5f, .5f, 2f);
+        }
+        else if (action == 0)
+        {
+            inter = Instantiate(intercept, new Vector3(-.5f, 1.3f, 1f), Quaternion.identity);
+            inter.transform.localScale = new Vector3(.5f, .5f, 2f);
+        }
+        else if (action == 3)
+        {
+            inter = Instantiate(intercept, new Vector3(.5f, 1.3f, 1f), Quaternion.identity);
+            inter.transform.localScale = new Vector3(.5f, .5f, 2f);
+        }
+        else
+        {
+            //inter.transform.localScale = new Vector3(.8f, .5f, 2f);
+        }
+        //inter.transform.SetParent(Cam.transform);
+        moveCam(move);
+        //Destroy(inter);
+    }
+
+    void exploit()
+    {
+        Debug.Log("Method: EXPLOIT");
+        for (int i = 0; i < 5; i++)
+        {
+            if (QSA[Guardian[1], Guardian[2]][i][0] == 1)
+            {
+                Debug.Log("MEMORY USED");
+                //Debug.Log(Guardian[1]);
+                move = QSA[Guardian[1], Guardian[2]][i];
+                //Debug.Log(move[0]);
+                //Debug.Log(i);
+
+                moveCam(move);
+                return;
+            }
+        }
+        Debug.Log("NO MEMORY");
+        move = QSA[Guardian[1], Guardian[2]][4];
+        moveCam(move);
+
+
+
+    }
+    void count()
+    {
+        int ctr = 0;
+        for (int i = 0; i < 19; i++)
+            for (int ii = 0; ii < 7; ii++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (QSA[i, ii][j][0] == 1)
+                    {
+                        ctr++;
+                    }
+                }
+            }
+        Debug.Log(ctr);
+    }
+}
---
+++ Assets/RandMove.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RandMove : MonoBehaviour
+{
+    private float movementDuration = 2.0f;
+    private float waitBeforeMoving = 2.0f;
+    private bool hasArrived = false;
+    public List<int> tick = new List<int>();
+    public GameObject AIRL;
+    private List<List<float>>[,] conn = new List<List<float>>[19,17];
+    private int action;
+    //private void
+
+    private void Start()
+    {
+        tick.Add(1);
+        tick.Add(0);
+        tick.Add(0);
+        conn = AIRL.GetComponent<RL>().QSA;
+    }
+
+    private void Update()
+    {
+        if (!hasArrived)
+        {
+            hasArrived = true;
+            GameObject lance = AIRL.GetComponent<RL>().inter;
+            Destroy(lance);
+            float randX = Random.Range(-.9f, 1f);
+            float randY = Random.Range(.7f, 1.4f);
+            StartCoroutine(MoveToPoint(new Vector3(randX, randY, 0.7f)));
+        }
+    }
+
+    private IEnumerator MoveToPoint(Vector3 targetPos)
+    {
+        float timer = 0.0f;
+        Vector3 startPos = transform.position;
+
+        while (timer < movementDuration)
+        {
+            timer += Time.deltaTime;
+            float t = timer / movementDuration;
+            t = t * t * t * (t * (6f * t - 15f) + 10f);
+            transform.position = Vector3.Lerp(startPos, targetPos, t);
+
+            yield return null;
+        }
+        tick[0] = 0;
+        targetPos[0] = (targetPos[0] * 10) +9;
+        int temp = (int)targetPos[0];
+        tick[1] = temp;
+        targetPos[1] = (targetPos[1] * 10) -7;
+        temp = (int)targetPos[1];
+        tick[2] = temp;
+        //Debug.Log("Wiggler");
+        //Debug.Log(tick[1]);
+        yield return new WaitForSeconds(waitBeforeMoving);
+        hasArrived = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Center(Clone)")
+        {
+            if (action != 7)
+            {
+                GameObject lance = AIRL.GetComponent<RL>().inter;
+                Destroy(lance);
+                
+                int action = AIRL.GetComponent<RL>().act;
+                //Debug.Log(action);
+                conn[tick[1], tick[2]][action][0] = 1f;
+
+            }
+        }
+    }
+}
+
